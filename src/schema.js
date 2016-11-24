a7e27b36be27baf54d50a43104708ae7b8c494f1
Shivam Kapoor <mail@shivamkapoor.com>
2016-11-24 13:35:28 +0530

    Defining queries: post, default

--- src/schema.js
+++ src/schema.js
@@ -1,51 +1,76 @@
 // import * as _ from 'underscore';
 
 // This is the Dataset in our blog
-// import PostsList from './data/posts';
+import PostsList from './data/posts';
 // import AuthorsList from './data/authors';
 // import {CommentList, ReplyList} from './data/comments';
 
 import {
-  // These are the basic GraphQL types
-  // GraphQLInt,
-  // GraphQLFloat,
-  GraphQLString,
-  // GraphQLList,
-  GraphQLObjectType,
-  // GraphQLEnumType,
+    // These are the basic GraphQL types
+    // GraphQLInt,
+    // GraphQLFloat,
+    GraphQLString,
+    GraphQLList,
+    GraphQLObjectType,
+    // GraphQLEnumType,
 
-  // This is used to create required fields and arguments
-  // GraphQLNonNull,
+    // This is used to create required fields and arguments
+    GraphQLNonNull,
 
-  // This is the class we need to create the schema
-  GraphQLSchema
+    // This is the class we need to create the schema
+    GraphQLSchema
 } from 'graphql';
 
 /**
   DEFINE YOUR TYPES BELOW
 **/
+const Post = new GraphQLObjectType({
+    name: 'Post',
+    description: 'This represents a Post',
+    fields: () => ({
+        _id: {
+            type: new GraphQLNonNull(GraphQLString)
+        },
+        title: {
+            type: new GraphQLNonNull(GraphQLString),
+            resolve: function (post) {
+                return post.title || "Doesn't exist";
+            }
+        },
+        content: {type: GraphQLString}
+    })
+});
 
 // This is the Root Query
 const Query = new GraphQLObjectType({
-  name: 'BlogSchema',
-  description: 'Root of the Blog Schema',
-  fields: () => ({
-    echo: {
-      type: GraphQLString,
-      description: 'Echo what you enter',
-      args: {
-        message: {type: GraphQLString}
-      },
-      resolve: function(source, {message}) {
-        return `received ${message}`;
-      }
-    }
-  })
+    name: 'BlogSchema',
+    description: 'Root of the Blog Schema',
+    fields: () => ({
+        posts: {
+            type: new GraphQLList(Post),
+            description: 'List of all the posts',
+            resolve: function() {
+                return PostsList;
+            }
+        },
+        echo: {
+            type: GraphQLString,
+            description: 'Echo what you enter',
+            args: {
+                message: {
+                    type: GraphQLString
+                }
+            },
+            resolve: function (source, {message}) {
+                return `received ${message}`;
+            }
+        }
+    })
 });
 
 // The Schema
 const Schema = new GraphQLSchema({
-  query: Query
+    query: Query
 });
 
 export default Schema;
